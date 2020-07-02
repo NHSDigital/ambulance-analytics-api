6cbb7337ea6190f0bf62889a28fafb4d307f3155
jalal <jalalhosseiny@gmail.com>
2020-07-02 15:53:19 +0100

    AMB-133 add apigee target server

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,8 +17,25 @@ module "ambulance-data" {
   name               = "ambulance-data"
   path               = "ambulance-data"
   apigee_environment = var.apigee_environment
-  proxy_type         = "sandbox"
+  proxy_type         = (var.force_sandbox || length(regexall("sandbox", var.apigee_environment)) > 0) ? "sandbox" : "live"
   namespace          = var.namespace
   make_api_product   = length(var.namespace) == 0
   api_product_display_name = length(var.namespace) > 0 ? "ambulance-data${var.namespace}" : "Ambulance Data Api"
+}
+
+resource "apigee_target_server" "ambulance-api" {
+    count   = (var.force_sandbox || length(regexall("sandbox", var.apigee_environment)) > 0) ? 0 : 1
+    name    = "dps-ambulance-api"
+    host    = var.dps-submission-api-host
+    env     = var.apigee_environment
+    enabled = true
+    port    = 443
+
+    ssl_info {
+        ssl_enabled              = true
+        client_auth_enabled      = false
+        ignore_validation_errors = false
+        ciphers                  = []
+        protocols                = []
+    }
 }
