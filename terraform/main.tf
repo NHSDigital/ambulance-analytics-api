9b30da64ff1bfed94bf73367990b7842fecc1a9a
jalal <jalalhosseiny@gmail.com>
2020-07-02 16:52:26 +0100

    AMB-133 reference old dps-submission-api server because of a problem with pipeline [workaround]

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,20 +22,3 @@ module "ambulance-data" {
   make_api_product   = length(var.namespace) == 0
   api_product_display_name = length(var.namespace) > 0 ? "ambulance-data${var.namespace}" : "Ambulance Data Api"
 }
-
-resource "apigee_target_server" "ambulance-api" {
-    count   = (var.force_sandbox || length(regexall("sandbox", var.apigee_environment)) > 0) ? 0 : 1
-    name    = "dps-ambulance-api"
-    host    = var.dps-submission-api-host
-    env     = var.apigee_environment
-    enabled = true
-    port    = 443
-
-    ssl_info {
-        ssl_enabled              = true
-        client_auth_enabled      = false
-        ignore_validation_errors = false
-        ciphers                  = []
-        protocols                = []
-    }
-}
