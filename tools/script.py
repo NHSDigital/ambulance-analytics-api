120beebe621fabeeb0d9187e561d740689c3f0e7
jalal <jalalhosseiny@gmail.com>
2020-09-03 12:33:26 +0100

    AMB-273 improve docker file and add make target

--- tools/script.py
+++ tools/script.py
@@ -1,8 +1,10 @@
 from authlib.jose import jwk
+import sys
 
 
 def main():
-    public_key = open('/files/jwtRS512.key.pub').read()
+    filename = sys.argv[1]
+    public_key = open(f'/files/{filename}').read()
     jwks = jwk.dumps(public_key, kty='RSA', crv_or_size=4096, alg='RS512')
     print(jwks)
 
