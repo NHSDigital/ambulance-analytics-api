55c7d9bdc43529d1dfe7bedd41269d68be5b3ae0
Adrian Clare <adrian.clare@airelogic.com>
2020-09-21 14:08:25 +0100

    chnages to sandbox

--- docker/ambulance-analytics-sandbox/src/routes/methodNotAllowed.js
+++ docker/ambulance-analytics-sandbox/src/routes/methodNotAllowed.js
@@ -1,13 +1,15 @@
 module.exports = [
     /**
-     * GET to /submission
+     * GET,PUT,DELETE,PATCH to /submission
      * If submissionResponse.json file exists in `mock` directory then that will be the response otherwise, 404
      */
     {
-      method: 'GET',
+      method: ['GET', 'PUT', 'DELETE', 'PATCH'],
       path: '/submission',
       handler: (request, h) => {
-        const responseMessage = `Request method must be POST, not ${request.route.method}`;
+        const responseMessage = {
+          error: `Request method must be POST, not ${request.route.method}`
+        } ;
         return h.response(responseMessage).code(405)
       }
     }
