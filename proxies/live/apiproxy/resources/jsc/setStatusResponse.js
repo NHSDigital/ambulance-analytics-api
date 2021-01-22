9dcbad799f768184bc5c4c2595042f326e840b11
jalal <jalalhosseiny@gmail.com>
2021-01-22 12:39:26 +0000

    AMB-468 fix typo

--- proxies/live/apiproxy/resources/jsc/setStatusResponse.js
+++ proxies/live/apiproxy/resources/jsc/setStatusResponse.js
@@ -1,5 +1,5 @@
 responseStatus = context.getVariable("response.status.code")
-if (responseStatus >= 200 && resosneStatus < 300) {
+if (responseStatus >= 200 && responseStatus < 300) {
   context.setVariable("health.total", "pass")
 } else {
   context.setVariable("health.total", "fail")
