33e2c3cdcc3ae1dfbb596f7e976bca31fa96855f
jalal <jalalhosseiny@gmail.com>
2021-02-12 14:18:59 +0000

    AMB-507 remove jwks flow and policies; this will be done in identity-service-api

--- proxies/live/apiproxy/resources/jsc/setServiceCalloutUrl.js
+++
@@ -1,7 +0,0 @@
-var urlvalue = context.getVariable('verifyapikey.VerifyAPIKey.FormParam.jwks-resource-url');
-var re = new RegExp('^(https?:\/\/[^/]+)(\/.*)$');
-var match = re.exec(urlvalue);
-if (match) {
-  context.setVariable('servicecallout.ServiceCallout.GetJWKS.target.url', match[1]);
-  context.setVariable('sc_urlPath', match[2]);
-}
